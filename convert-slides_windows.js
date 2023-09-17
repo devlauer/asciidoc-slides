599419bb5b2808b462b1a14ac55516b4b24b2e5b
dev.lauer <dev.lauer@elnarion.de>
2023-09-17 21:59:16 +0200

    add separate windows convert-slides.js

--- convert-slides_windows.js
+++ convert-slides_windows.js
@@ -1,3 +1,4 @@
+// Load Asciidoctor.js and the reveal.js converter
 var asciidoctor = require('@asciidoctor/core')()
 var asciidoctorRevealjs = require('@asciidoctor/reveal.js')
 const fse = require('fs-extra');
@@ -18,6 +19,7 @@ const destDir = `./build/slides/`;
 
 fs.rmSync(destDir, {recursive: true, force: true});
 
+// Convert the document 'presentation.adoc' using the reveal.js converter
 var options = {safe: 'safe', backend: 'revealjs', to_dir: destDir, imagesdir: srcDir.concat('images/'), mkdirs: true}
 asciidoctor.convertFile(srcDir.concat('linux-container.adoc'), options)
 
